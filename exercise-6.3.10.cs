85f2f52962ac163ba3fae2b9acbdadabe87afe65
Nagodo <89977444+Nagodo@users.noreply.github.com>
2025-09-11 08:54:29 +0200

    exercises

--- exercise-6.3.10.cs
+++ exercise-6.3.10.cs
@@ -8,7 +8,7 @@
         int primeAmount = 0;
         for (int i = 0; i < 100000; i++) {
 
-            if (!IsPrime(i)) continue; 
+            if (!i.IsPrime()) continue; 
 
             if (i > largestPrime) largestPrime = i;
             primeAmount++;
@@ -17,8 +17,8 @@
         Console.WriteLine($"Largest: {largestPrime}. Amount: {primeAmount}");
     }
 
-    public static bool IsPrime(int number) {
 
+    public static bool IsPrime(this int number) {
         if (number == 1 || number == 0) return false;
 
         int maxDivider = number / 2;
